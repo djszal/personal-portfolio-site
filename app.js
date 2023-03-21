2691dec921ae903e9a51542c04340a4047ce9dc8
Dylan <szalank.dj@gmail.com>
2023-03-21 17:43:32 -0400

    working on getting input values added to state in js file

--- app.js
+++ app.js
@@ -7,9 +7,24 @@ state.contact =
 const body = document.body
 
 const sendButton = document.querySelector(".button")
+const nameInput = document.querySelector("input[name=Name]");
+const emailInput = document.querySelector("input[name=Email]");
+const messageInput = document.querySelector("textarea[name=Message]");
+
+console.log(messageInput)
 
 const sendEmail = () => {
-    console.log("Hello")
+    
+    const nameValue = nameInput.value;
+    const emailValue = emailInput.value;
+    const messageValue = messageInput.value;
+    
+    state.contact.name = nameValue
+    state.contact.email = emailValue
+    state.contact.message = messageValue
+    
+    console.log(state.contact)
+    
 }
 
 sendButton.addEventListener("click", sendEmail)
